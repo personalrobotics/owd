78568d24b06513facd7fa6024c6fb8611ffe9a0f
Mike Vande Weghe <vandeweg@cmu.edu>
2011-04-19 19:56:10 +0000

    renamed sample package to make it clearer

--- MyTraj.hh
+++ MyTraj.hh
@@ -2,22 +2,32 @@
 #define MYTRAJ_HH
 
 #include <openwam/Trajectory.hh>
-#include "owd_traj_example/AddMyTrajectory.h"
+#include <openwam/Plugin.hh>
+#include <owd_traj_example/AddMyTrajectory.h>
 #include <ros/ros.h>
 
-bool register_trajectory_plugin();
-
-class MyTraj : public Trajectory {
+class MyTraj : public OWD::Trajectory {
 public:
-  MyTraj(int joint, double distance);
-  ~MyTraj();
-  virtual void evaluate(double y[], double yd[], double ydd[], double dt);
+  MyTraj(int joint, double torque);
+  virtual void evaluate(OWD::Trajectory::TrajControl &tc, double dt);
   static bool AddTrajectory(owd_traj_example::AddMyTrajectory::Request &req,
 			    owd_traj_example::AddMyTrajectory::Response &res);
-
   static ros::ServiceServer ss_Add;
+  static bool Register();
+  static void Shutdown();
 
+private:
+  int joint;
+  double torque;
 };
 
+class MyPlugin : public OWD::Plugin {
+public:
+  MyPlugin();
+  ~MyPlugin();
+  virtual void Publish();
+
+  ros::Publisher  pub_info;
+};
 
 #endif // MYTRAJ_HH
