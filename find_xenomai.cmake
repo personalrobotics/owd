df3d9329011523d3152659892a474c5dd3054910
Mike Vande Weghe <vandeweg@cmu.edu>
2011-05-11 16:08:19 +0000

    Made the RT versions optional based on whether or not Xenomai is found on the system

--- find_xenomai.cmake
+++ find_xenomai.cmake
@@ -6,20 +6,26 @@ if (XENO-CONFIG)
                    OUTPUT_STRIP_TRAILING_WHITESPACE)
    if (XENOMAI_NOTFOUND)
        message (SEND_ERROR "Could not find Xenomai include files (command xeno-config --xeno-cflags failed)\n")
+   else (XENOMAI_NOTFOUND)
+     execute_process(COMMAND ${XENO-CONFIG} --xeno-ldflags
+                     RESULT_VARIABLE XENOMAI_NOTFOUND2
+		     OUTPUT_VARIABLE RT_LIBS
+                     OUTPUT_STRIP_TRAILING_WHITESPACE)
+     if (XENOMAI_NOTFOUND2)
+       message (SEND_ERROR "Could not find Xenomai libs (command xeno-config --xeno-ldflags failed)\n")
+     else (XENOMAI_NOTFOUND2)
+       set (RT_BUILD "true")
+     endif (XENOMAI_NOTFOUND2)
    endif(XENOMAI_NOTFOUND)
-   execute_process(COMMAND ${XENO-CONFIG} --xeno-ldflags
-                   RESULT_VARIABLE XENOMAI_NOTFOUND
-                   OUTPUT_VARIABLE RT_LIBS
-                   OUTPUT_STRIP_TRAILING_WHITESPACE)
-   if (XENOMAI_NOTFOUND)
-      message (SEND_ERROR "Could not find Xenomai libs (command xeno-config --xeno-ldflags failed)\n")
-   endif (XENOMAI_NOTFOUND)
-   
 elseif ("$ENV{XENOMAI_PATH}" STRGREATER "")
-message(STATUS "Using XENOMAI_PATH=$ENV{XENOMAI_PATH}")
+   message(STATUS "Using XENOMAI_PATH=$ENV{XENOMAI_PATH}")
    set (RT_DEFS "-I$ENV{XENOMAI_PATH}/include")
    set (RT_LIBS "-L$ENV{XENOMAI_PATH}/lib -lnative")
+   set (RT_BUILD "true")
 endif (XENO-CONFIG)
 
-set (RT_DEFS "-DOWD_RT ${RT_DEFS}")
-set (RT_LIBS "-lnative ${RT_LIBS}")
+if (RT_BUILD)
+  set (RT_DEFS "-DOWD_RT ${RT_DEFS}")
+  set (RT_LIBS "-lnative ${RT_LIBS}")
+endif (RT_BUILD)
+
