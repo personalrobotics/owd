4bf57cf201039506e91b0c245be258eb454dff61
Michael Koval <mkoval@cs.cmu.edu>
2014-06-14 08:41:23 +0000

    Finished Catkin-izing openwam and openmath.

--- catkin.cmake
+++ catkin.cmake
@@ -44,11 +44,106 @@ else ()
     set(CANBUS_LIBS "")
 endif ()
 
-include_directories(${catkin_INCLUDE_DIRS})
+include_directories(
+    openwam
+    openmath
+    ${catkin_INCLUDE_DIRS}
+)
 
 add_subdirectory(openmath)
 add_subdirectory(openwam)
-#add_subdirectory(${PROJECT_SOURCE_DIR}/openmath)
+
+set(OWD_SOURCE 
+    owd.cpp
+    openwamdriver.cpp
+    bhd280.cc
+    ft.cc
+    tactile.cc
+)
+set(OWD_LIBS
+    wamcan
+    openwam
+    openmath
+    lapack
+    blas
+    gfortran
+    ${CANBUS_LIBS}
+)
+
+if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+    add_executable(owd ${OWD_SOURCE})
+    target_link_libraries(owd ${OWD_LIBS})
+    set_target_properties(owd PROPERTIES
+        COMPILE_FLAGS "${CANBUS_DEFS}"
+        LINK_FLAGS "${CANBUS_LDFLAGS}"
+    )
+
+    add_executable(canbhd ${OWD_SOURCE})
+    target_link_libraries(canbhd ${OWD_LIBS})
+    set_target_properties(canbhd PROPERTIES
+        COMPILE_FLAGS "${CANBUS_DEFS} -DBH280 -DBH280_ONLY"
+        LINK_FLAGS "${CANBUS_LDFLAGS}"
+    )
+endif ()
+
+#rosbuild_add_executable(owdsim owd.cpp openwamdriver.cpp)
+#target_link_libraries(owdsim openwamsim openmath lapack blas gfortran)
+#rosbuild_add_compile_flags(owdsim "-DOWDSIM")
+#
+#if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#rosbuild_add_executable(owd_test test.cpp)
+#rosbuild_add_executable(synctest synctest.cpp)
+#
+#rosbuild_add_executable(puck_update puck_update.cpp)
+#target_link_libraries(puck_update wamcan openwam ${CANBUS_LIBS})
+#rosbuild_add_compile_flags(puck_update "${CANBUS_DEFS}")
+#rosbuild_add_link_flags(puck_update "${CANBUS_LDFLAGS}")
+#
+#rosbuild_add_executable(puck_defaults puck_defaults.cpp)
+#target_link_libraries(puck_defaults wamcan openwam ${CANBUS_LIBS})
+#rosbuild_add_compile_flags(puck_defaults "${CANBUS_DEFS}")
+#rosbuild_add_link_flags(puck_defaults "${CANBUS_LDFLAGS}")
+#
+#rosbuild_add_executable(puck_getprops puck_getprops.cpp)
+#target_link_libraries(puck_getprops wamcan openwam ${CANBUS_LIBS})
+#rosbuild_add_compile_flags(puck_getprops "${CANBUS_DEFS}")
+#rosbuild_add_link_flags(puck_getprops "${CANBUS_LDFLAGS}")
+#
+#rosbuild_add_executable(puck_find_mofst puck_find_mofst.cpp)
+#target_link_libraries(puck_find_mofst wamcan openwam ${CANBUS_LIBS})
+#rosbuild_add_compile_flags(puck_find_mofst "${CANBUS_DEFS}")
+#rosbuild_add_link_flags(puck_find_mofst "${CANBUS_LDFLAGS}")
+#
+#rosbuild_add_executable(puck_setprop puck_setprop.cpp)
+#target_link_libraries(puck_setprop wamcan openwam ${CANBUS_LIBS})
+#rosbuild_add_compile_flags(puck_setprop "${CANBUS_DEFS}")
+#rosbuild_add_link_flags(puck_setprop "${CANBUS_LDFLAGS}")
+#endif (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#
+#rosbuild_add_executable(owd_traj_timer owd_traj_timer.cpp)
+#if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#  target_link_libraries(owd_traj_timer openwam)
+#else (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#  target_link_libraries(owd_traj_timer openwamsim)
+#endif (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#
+#if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
+#if (RT_BUILD)
+#  rosbuild_add_executable(owdrt owd.cpp openwamdriver.cpp bhd280.cc
+#                                ft.cc tactile.cc)
+#  target_link_libraries(owdrt wamcanrt openwam openmath lapack blas
+#                              gfortran ${CANBUS_LIBS} ${RT_LIBS})
+#  rosbuild_add_compile_flags(owdrt "${CANBUS_DEFS} ${RT_DEFS}")
+#  rosbuild_add_link_flags(owdrt "${CANBUS_LDFLAGS}")
+#
+#  rosbuild_add_executable(canbhdrt owd.cpp openwamdriver.cpp bhd280.cc
+#                                   ft.cc tactile.cc)
+#  target_link_libraries(canbhdrt bhdcanrt openwam openmath lapack blas
+#      	                         gfortran ${CANBUS_LIBS} ${RT_LIBS})
+#  rosbuild_add_compile_flags(canbhdrt "-DBH280 -DBH280_ONLY ${CANBUS_DEFS} ${RT_DEFS}")
+#  rosbuild_add_link_flags(canbhdrt "${CANBUS_LDFLAGS}")
+#endif (RT_BUILD)
+#endif (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
 
 #include_directories(openwam openmath ${catkin_INCLUDE_DIRS})
 #link_libraries(${catkin_LIBRARIES})
--- openwam/catkin.cmake
+++ openwam/catkin.cmake
@@ -1,41 +1,11 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
-    add_library(openwam
-        Joint.cc
-        Motor.cc
-        Puck.cc
-        Group.cc
-        Sigmoid.cc
-        Kinematics.cc
-        Dynamics.cc
-        TrajType.cc
-        PulseTraj.cc
-        Trajectory.cc
-        ParabolicSegment.cc
-        ParaJointTraj.cc
-        MacJointTraj.cc
-        MacQuinticBlend.cc
-        MacQuinticSegment.cc
-        ServoTraj.cc
-        StepTraj.cc
-        ConstrainedForceTrajectory.cc 
-        butterworth_solver.c
-        MultiSync.cc
-        BinaryData.cc
-        JSController.cc
-    )
-endif ()
-
-add_library(openwamsim
-    CANbus_sim.cc
+set(OPENWAM_SOURCE
     Joint.cc
     Motor.cc
     Puck.cc
     Group.cc
-    ControlLoop.cc
     Sigmoid.cc
-    WAM.cc
     Kinematics.cc
     Dynamics.cc
     TrajType.cc
@@ -48,63 +18,60 @@ add_library(openwamsim
     MacQuinticSegment.cc
     ServoTraj.cc
     StepTraj.cc
-    ConstrainedForceTrajectory.cc
-    Plugin.cc
+    ConstrainedForceTrajectory.cc 
     butterworth_solver.c
     MultiSync.cc
     BinaryData.cc
     JSController.cc
 )
-target_link_libraries(openwamsim ${CANBUS_LIBS})
-set_target_properties(openwamsim PROPERTIES
-    COMPILE_FLAGS "${CANBUS_DEFS} -DOWDSIM"
+set(OPENWAM_IMPL_SOURCE
+    CANbus_sim.cc
+    Plugin.cc
+    WAM.cc
+    ControlLoop.cc
 )
 
 if (CANBUS_TYPE STREQUAL "ESD" OR CANBUS_TYPE STREQUAL "PEAK")
-    add_library(wamcan
-        ControlLoop.cc
-        CANbus.cc
-        WAM.cc
-        Plugin.cc
-    )
-    target_link_libraries(wamcan ${CANBUS_LIBS})
-    set_target_properties(wamcan PROPERTIES COMPILE_FLAGS ${CANBUS_DEFS})
+    add_library(openwam ${OPENWAM_SOURCE})
 
-    add_library(bhdcan
-        ControlLoop.cc
-        CANbus.cc
-        WAM.cc
-        Plugin.cc
+    add_library(wamcan ${OPENWAM_IMPL_SOURCE})
+    target_link_libraries(wamcan ${CANBUS_LIBS})
+    set_target_properties(wamcan PROPERTIES
+        COMPILE_FLAGS "${CANBUS_DEFS}"
+        LINK_FLAGS "${CANBUS_LDFLAGS}"
     )
+
+    add_library(bhdcan ${OPENWAM_IMPL_SOURCE})
     target_link_libraries(bhdcan ${CANBUS_LIBS})
     set_target_properties(bhdcan PROPERTIES
         COMPILE_FLAGS "${CANBUS_DEFS} -DBH280_ONLY"
+        LINK_FLAGS "${CANBUS_LDFLAGS}"
     )
 
     if (RT_BUILD)
-        add_library(wamcanrt
-            ControlLoop.cc
-            CANbus.cc
-            WAM.cc
-            Plugin.cc
-        )
+        add_library(wamcanrt ${OPENWAM_IMPL_SOURCE})
         target_link_libraries(wamcanrt ${CANBUS_LIBS} ${RT_LIBS})
         set_target_properties(wamcanrt PROPERTIES
             COMPILE_FLAGS "${CANBUS_DEFS} ${RT_DEFS}"
+            LINK_FLAGS "${CANBUS_LDFLAGS}"
         )
 
-        add_library(bhdcanrt
-            ControlLoop.cc
-            CANbus.cc
-            WAM.cc
-            Plugin.cc
-        )
+        add_library(bhdcanrt ${OPENWAM_IMPL_SOURCE})
         target_link_libraries(bhdcanrt ${CANBUS_LIBS} ${RT_LIBS})
         set_target_properties(bhdcanrt PROPERTIES
             COMPILE_FLAGS "${CANBUS_DEFS} ${RT_DEFS} -DBH280_ONLY"
+            LINK_FLAGS "${CANBUS_LDFLAGS}"
         )
     endif ()
 endif ()
 
+add_library(openwamsim ${OPENWAM_SOURCE} ${OPENWAM_IMPL_SOURCE})
+target_link_libraries(openwamsim ${CANBUS_LIBS})
+set_target_properties(openwamsim PROPERTIES
+    COMPILE_FLAGS "${CANBUS_DEFS} -DOWDSIM"
+    LINK_FLAGS "${CANBUS_LDFLAGS}"
+)
+
+# TODO: Something needs to link with openmath.
 #add_executable(MacTrajTest MacTrajTest.cc)
 #target_link_libraries(MacTrajTest openwamsim)
